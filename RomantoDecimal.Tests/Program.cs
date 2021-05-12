7238ddd33b4e7b333821d1e6d04a898d28ed3f53
Okinz <steven14812@gmail.com>
2021-05-12 15:59:57 -0400

    Added test for convertNum

---
+++ RomantoDecimal.Tests/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace RomantoDecimal.Tests
+{
+  public class Program
+  {
+    [Fact]
+    public void Should_returnDecimalInt()
+    {
+      // //Assemble
+      // string romanNum = "X";
+      // //Act
+      // int integerNum = ConvertRoman(romanNum);
+      // //Assert
+      // integerNum.Should().Be(10);
+    }
+  }
+}
