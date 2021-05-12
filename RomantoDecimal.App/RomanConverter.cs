44402c5d70fe5c59a73967cc9398628e511c8f5c
LWooler <lwooler@att.net>
2021-05-12 15:45:38 -0500

    added addition to roman numeral algorithm

--- RomantoDecimal.App/RomanConverter.cs
+++ RomantoDecimal.App/RomanConverter.cs
@@ -5,19 +5,27 @@ namespace RomantoDecimal.App
 {
     public class RomanConverter
     {
-        public static int ConvertRoman(string roman) {
-
-            Dictionary<string, int> romanNumerals =
-            new Dictionary<string, int>(){
-                {"I",1},
-                {"V", 5},
-                {"X", 10},
-                {"L", 50},
-                {"C", 100},
-                {"D", 500},
-                {"M", 1000}
+        public static Dictionary<char, int> romanNumerals =
+            new Dictionary<char, int>(){
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
             };
-            return romanNumerals[roman];
+        public static int ConvertRoman(string roman)
+        {
+            int summation = 0;
+
+            foreach (var letter in roman)
+            {
+                summation += romanNumerals[letter];
+            }
+
+
+            return summation;
         }
     }
 }
--- RomantoDecimal.Tests/RomanConverterTest.cs
+++ RomantoDecimal.Tests/RomanConverterTest.cs
@@ -5,28 +5,39 @@ using RomantoDecimal.App;
 
 namespace RomantoDecimal.Tests
 {
-  public class RomanConverterTest
-  {
-    [Fact]
-    public void Should_returnDecimalInt()
+    public class RomanConverterTest
     {
-      //Assemble
-      string romanNum = "X";
-      //Act
-      int integerNum = RomanConverter.ConvertRoman(romanNum);
-      //Assert
-      integerNum.Should().Be(10);
-    }
+        [Fact]
+        public void Should_returnDecimalInt()
+        {
+            //Assemble
+            string romanNum = "X";
+            //Act
+            int integerNum = RomanConverter.ConvertRoman(romanNum);
+            //Assert
+            integerNum.Should().Be(10);
+        }
 
-    [Fact]
-    public void Should_ReturnSumOfNumerals()
-    {
-    // //Given
-    // string romanNum = "XVI";
-    // //When
-    // int integerNum = RomanConverter.ConvertRoman(romanNum);
-    // //Then
-    // integerNum.Should().Be(16);
-    }
+        [Fact]
+        public void Should_ReturnSumOfNumerals()
+        {
+            //Given
+            string romanNum = "XVI";
+            //When
+            int integerNum = RomanConverter.ConvertRoman(romanNum);
+            //Then
+            integerNum.Should().Be(16);
+        }
+
+        [Fact]
+        public void Should_ReturnSubtractionOfNumerals()
+        {
+          //Given
+          string romanNum = "XIV";
+          //When
+          int integerNum = RomanConverter.ConvertRoman(romanNum);
+          //Then
+          // integerNum.Should().Be(14);
+        }
   }
 }
