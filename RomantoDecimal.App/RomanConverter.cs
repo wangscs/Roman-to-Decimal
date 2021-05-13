825663c20da7966d83ed26b8d10a6d149344a451
Okinz <steven14812@gmail.com>
2021-05-13 10:24:30 -0400

    Completed empty parameter test and started lower case test

--- RomantoDecimal.App/RomanConverter.cs
+++ RomantoDecimal.App/RomanConverter.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 
 namespace RomantoDecimal.App
 {
-    public class RomanConverter
-    {
-        public static Dictionary<char, int> romanNumerals =
-            new Dictionary<char, int>(){
+  public class RomanConverter
+  {
+    public static Dictionary<char, int> romanNumerals =
+        new Dictionary<char, int>(){
                 {'I', 1},
                 {'V', 5},
                 {'X', 10},
@@ -14,41 +14,37 @@ namespace RomantoDecimal.App
                 {'C', 100},
                 {'D', 500},
                 {'M', 1000}
-            };
-        public static int ConvertRoman(string roman)
+        };
+    public static int ConvertRoman(string roman)
+    {
+      int summation = 0;
+
+      for (int i = 0; i < roman.Length; i++)
+      {
+        int current = romanNumerals[roman[i]];
+        int next = current;
+        if (i != roman.Length - 1)
         {
-            int summation = 0;
-
-            // foreach (var letter in roman)
-            // {
-            //     summation += romanNumerals[letter];
-            // }
-            for (int i = 0; i < roman.Length; i++)
-            {
-                int current = romanNumerals[roman[i]];
-                int next = current;
-                if (i != roman.Length - 1)
-                {
-                    next = romanNumerals[roman[i + 1]];
-                }
-
-                if (current < next)
-                {
-                    summation += (next - current);
-                    i++;
-                }
-                else
-                {
-                    summation += current;
-                }
-
-            }
-            if (summation > 10000)
-            {
-                throw new ApplicationException("Number exceeds max value");
-            }
-
-            return summation;
+          next = romanNumerals[roman[i + 1]];
         }
+
+        if (current < next)
+        {
+          summation += (next - current);
+          i++;
+        }
+        else
+        {
+          summation += current;
+        }
+
+      }
+      if (summation > 10000)
+      {
+        throw new ApplicationException("Number exceeds max value");
+      }
+
+      return summation;
     }
+  }
 }
--- RomantoDecimal.Tests/RomanConverterTest.cs
+++ RomantoDecimal.Tests/RomanConverterTest.cs
@@ -5,66 +5,88 @@ using RomantoDecimal.App;
 
 namespace RomantoDecimal.Tests
 {
-    public class RomanConverterTest
+  public class RomanConverterTest
+  {
+    [Fact]
+    public void Should_ReturnZero_IfParameterIsEmpty()
     {
-        [Fact]
-        public void Should_returnDecimalInt()
-        {
-            //Assemble
-            string romanNum = "X";
-            //Act
-            int integerNum = RomanConverter.ConvertRoman(romanNum);
-            //Assert
-            integerNum.Should().Be(10);
-        }
-
-        [Fact]
-        public void Should_ReturnSumOfNumerals()
-        {
-            //Given
-            string romanNum = "XVI";
-            //When
-            int integerNum = RomanConverter.ConvertRoman(romanNum);
-            //Then
-            integerNum.Should().Be(16);
-        }
-
-        [Theory]
-        [InlineData("MCMXLIV", 1944)]
-        [InlineData("XIV", 14)]
-        public void Should_ReturnSubtractionOfNumerals(string romanNum, int answer)
-        {
-            //Given
-            //When
-            int integerNum = RomanConverter.ConvertRoman(romanNum);
-            //Then
-            integerNum.Should().Be(answer);
-        }
-
-        [Fact]
-        public void Should_ThrowError_WhenNumberExceedsMaxValueOf10000()
-        {
-            //Given
-            string romanNum = "MMMMMMMMMMM";
-
-            //When
-            Action action = () => RomanConverter.ConvertRoman(romanNum);
-
-            //Then
-            action.Should().Throw<ApplicationException>().WithMessage("Number exceeds max value");
-        }
-
-        [Fact]
-        public void Should_ThrowException_WhenInputIsNull()
-        {
-            //Given
-            string romanNum = null;
-
-            //When
-            Action action = () => RomanConverter.ConvertRoman(romanNum);
-
-            //Then
-            action.Should().Throw<NullReferenceException>();
-        }
+      //Given
+      string romanNum = "";
+      //When
+      int integerNum = RomanConverter.ConvertRoman(romanNum);
+      //Then
+      integerNum.Should().Be(0);
     }
+
+    [Fact]
+    public void Should_returnDecimalInt()
+    {
+      //Assemble
+      string romanNum = "X";
+      //Act
+      int integerNum = RomanConverter.ConvertRoman(romanNum);
+      //Assert
+      integerNum.Should().Be(10);
+    }
+
+    [Fact]
+    public void Should_ReturnSumOfNumerals()
+    {
+      //Given
+      string romanNum = "XVI";
+      //When
+      int integerNum = RomanConverter.ConvertRoman(romanNum);
+      //Then
+      integerNum.Should().Be(16);
+    }
+
+    [Theory]
+    [InlineData("MCMXLIV", 1944)]
+    [InlineData("XIV", 14)]
+    public void Should_ReturnSubtractionOfNumerals(string romanNum, int answer)
+    {
+      //Given
+      //When
+      int integerNum = RomanConverter.ConvertRoman(romanNum);
+      //Then
+      integerNum.Should().Be(answer);
+    }
+
+    [Fact]
+    public void Should_ThrowError_WhenNumberExceedsMaxValueOf10000()
+    {
+      //Given
+      string romanNum = "MMMMMMMMMMM";
+
+      //When
+      Action action = () => RomanConverter.ConvertRoman(romanNum);
+
+      //Then
+      action.Should().Throw<ApplicationException>().WithMessage("Number exceeds max value");
+    }
+
+    [Fact]
+    public void Should_ThrowException_WhenInputIsNull()
+    {
+      //Given
+      string romanNum = null;
+
+      //When
+      Action action = () => RomanConverter.ConvertRoman(romanNum);
+
+      //Then
+      action.Should().Throw<NullReferenceException>();
+    }
+
+    [Fact]
+    public void Should_ReturnNum_EvenWithLowerCaseParams()
+    {
+      //Given
+      string romanNum = "ix";
+      //When
+      int integerNum = RomanConverter.ConvertRoman(romanNum);
+      //Then
+      integerNum.Should().Be(9);
+    }
+  }
 }
